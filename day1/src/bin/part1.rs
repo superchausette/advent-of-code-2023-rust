6e81cf181c8a05df7785bb02de1492933f26a3ea
Superchausette <superchausette@superchausette.fr>
2023-12-06 00:10:56 +0100

    Day 2 - Part 2

--- day1/src/bin/part1.rs
+++ day1/src/bin/part1.rs
@@ -7,27 +7,28 @@ fn main() {
 }
 
 fn part1(input: &str) -> u64 {
-    let mut calibration_value: u64 = 0;
-    for line in input.lines() {
-        let mut first_and_last: (Option<u8>, Option<u8>) = (None, None);
-        for char in line.chars() {
-            if !char.is_digit(10) {
-                continue;
-            }
-            let digit = char as u8 - '0' as u8;
-            if first_and_last.0.is_none() {
-                first_and_last.0 = Some(digit);
-            }
-            first_and_last.1 = Some(char as u8 - '0' as u8);
+    input.lines().map(process_line).fold(0, |acc, val| acc + val)
+}
+
+fn process_line(line: &str) -> u64 {
+    let mut first_and_last: (Option<u8>, Option<u8>) = (None, None);
+    for char in line.chars() {
+        if !char.is_ascii_digit() {
+            continue;
         }
-        match first_and_last {
-            (Some(first), None) => calibration_value += first as u64,
-            (Some(first), Some(second)) => calibration_value += first as u64 * 10 + second as u64,
-            (None, Some(_)) => panic!("Something went wrong 😟"),
-            (None, None) => (),
+        let digit = char as u8 - b'0';
+        if first_and_last.0.is_none() {
+            first_and_last.0 = Some(digit);
         }
+        first_and_last.1 = Some(char as u8 - b'0');
     }
-    return calibration_value;
+    match first_and_last {
+        (Some(first), None) => first as u64,
+        (Some(first), Some(second)) => first as u64 * 10 + second as u64,
+        (None, Some(_)) => panic!("Something went wrong 😟"),
+        (None, None) =>  0,
+    }
+
 }
 
 #[cfg(test)]
--- day1/src/bin/part2.rs
+++ day1/src/bin/part2.rs
@@ -1,11 +1,89 @@
+use core::panic;
+
 fn main() {
-    let input = include_str!("part2.txt");
-    let out = part1(input);
+    let input = include_str!("part1.txt");
+    let out = part2(input);
     dbg!(out);
 }
 
-fn part1(input: &str) -> String {
-    return "TODO".to_string();
+struct Lexer<'a> {
+    line: &'a str,
+}
+
+impl<'a> Lexer<'a> {
+    fn read_next_digit(&mut self) -> Option<u8> {
+        if self.line.is_empty() {
+            return None;
+        }
+        let char = self.line.chars().nth(0).expect("Where is my char ?");
+        if char > '0' && char <= '9' {
+            self.line = &self.line[1..];
+            return Some(char as u8 - b'0');
+        }
+        if self.line.starts_with("one") {
+            self.line = &self.line[1..];
+            return Some(1);
+        }
+        if self.line.starts_with("two") {
+            self.line = &self.line[1..];
+            return Some(2);
+        }
+        if self.line.starts_with("three") {
+            self.line = &self.line[1..];
+            return Some(3);
+        }
+        if self.line.starts_with("four") {
+            self.line = &self.line[1..];
+            return Some(4);
+        }
+        if self.line.starts_with("five") {
+            self.line = &self.line[1..];
+            return Some(5);
+        }
+        if self.line.starts_with("six") {
+            self.line = &self.line[1..];
+            return Some(6);
+        }
+        if self.line.starts_with("seven") {
+            self.line = &self.line[1..];
+            return Some(7);
+        }
+        if self.line.starts_with("eight") {
+            self.line = &self.line[1..];
+            return Some(8);
+        }
+        if self.line.starts_with("nine") {
+            self.line = &self.line[1..];
+            return Some(9);
+        }
+        self.line = &self.line[1..];
+        self.read_next_digit()
+    }
+}
+
+fn process_line(line: &str) -> u64 {
+    let mut first_and_last: (Option<u8>, Option<u8>) = (None, None);
+
+    let mut lexer = Lexer { line };
+    while let Some(digit) = lexer.read_next_digit() {
+        if first_and_last.0.is_none() {
+            first_and_last.0 = Some(digit);
+        }
+        first_and_last.1 = Some(digit);
+    }
+    match first_and_last {
+        (Some(first), None) => first as u64,
+        (Some(first), Some(second)) => first as u64 * 10 + second as u64,
+        (None, Some(_)) => panic!("Something went wrong 😟"),
+        (None, None) => 0,
+    }
+}
+
+fn part2(input: &str) -> u64 {
+    input
+        .lines()
+        .map(process_line)
+        .fold(0, |acc, val| acc + val)
 }
 
 #[cfg(test)]
@@ -13,8 +91,37 @@ mod test {
     use super::*;
     #[test]
     fn example() {
-        let expected_result = "OrNotTODO";
-        let result = part1("");
+        let expected_result = 281;
+        let result = part2(
+            "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen",
+        );
         assert_eq!(result, expected_result);
+    }
+    #[test]
+    fn example_lines() {
+        assert_eq!(29, process_line("two1nine"));
+        assert_eq!(83, process_line("eightwothree"));
+        assert_eq!(13, process_line("abcone2threexyz"));
+        assert_eq!(24, process_line("xtwone3four"));
+        assert_eq!(42, process_line("4nineeightseven2"));
+        assert_eq!(14, process_line("zoneight234"));
+        assert_eq!(76, process_line("7pqrstsixteen"));
+    }
+
+    #[test]
+    fn example_edge_cases() {
+        assert_eq!(18, process_line("oneight"));
+        assert_eq!(11, process_line("one7one"));
+        assert_eq!(78, process_line("sevenineight"));
+        assert_eq!(82, process_line("eightwo"));
+        assert_eq!(51, process_line("fivezg8jmf6hrxnhgxxttwoneg"));
+        assert_eq!(79, process_line("fplrjjznseventwocrv9"));
+        assert_eq!(95, process_line("977ckpkmx5"));
     }
 }
